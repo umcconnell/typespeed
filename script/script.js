c2c95fd09d8bb6ab5fbaeb2b410d7266541c98b1
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-14 15:21:09 +0200

    Import api.js in script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
+import { fetchWords } from "./api.js";
+
 export function setupCanvas(canvas) {
     let [width, height] = [window.innerWidth, window.innerHeight - 100];
-    
+
     canvas.width = width;
     canvas.height = height;
 
