e1acb00a0615bb74eeeedf39b1b33634d7bd1d70
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-14 15:38:42 +0200

    Add randomInRange helper

--- script/helpers.js
+++ script/helpers.js
@@ -1,3 +1,7 @@
 export function randomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
+
+export function randomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
