06636c762057e6dfb958edf7037e54eea6e3b0d9
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-14 17:51:52 +0200

    Add config file as configuration and store
    Enable state sharing through the application modules by using a
    store model.
    Additionally, provide base settings and configurations.

---
+++ script/config.js
@@ -0,0 +1,36 @@
+export const fontFamilies = [
+    "serif",
+    "sans-serif",
+    "cursive",
+    "fantasy",
+    "monospace"
+];
+
+export let canvasEnv = {
+    canvas: undefined,
+    ctx: undefined,
+    width: undefined,
+    height: undefined,
+    animationFrame: undefined
+};
+
+export let gameEnv = {
+    isFetching: false,
+    isPaused: true,
+    activeWords: [],
+    availableWords: [],
+    lives: 3,
+    score: 0,
+    verticalSpeed: undefined,
+    wordAppearDelay: undefined,
+    currWordAppearDelay: undefined
+};
+
+export let config = {
+    initialVerticalSpeed: 0.5,
+    initialWordAppearDelay: 350,
+    wordAppearDelayDecrease: 15,
+    mainColor: "#339af0",
+    fontFamily: "auto",
+    fontSize: "auto"
+};
