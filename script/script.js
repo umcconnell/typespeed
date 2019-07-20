81b0c0030f228cec3094811fa985e7141c6a89cc
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-20 19:05:52 +0200

    Add play and pause methods

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 import { fetchWords, changeFetchWordStatus } from "./api.js";
-import { canvasEnv } from "./config.js";
+import { canvasEnv, gameEnv } from "./config.js";
+import { draw } from "./gameLogic.js";
 
 export function setupCanvas(canvas) {
     let [width, height] = [window.innerWidth, window.innerHeight - 100];
@@ -13,4 +14,18 @@ export function setupCanvas(canvas) {
     canvasEnv.height = height;
 
     return [width, height];
+}
+
+export function play() {
+    if (!gameEnv.isPaused) return;
+
+    canvasEnv.animationFrame = requestAnimationFrame(draw);
+    gameEnv.isPaused = false;
+}
+
+export function pause() {
+    if (gameEnv.isPaused) return;
+
+    canvasEnv.animationFrame = cancelAnimationFrame(canvasEnv.animationFrame);
+    gameEnv.isPaused = true;
 }
