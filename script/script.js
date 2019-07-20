c0ce7b65023610e30e53fb78f31bad1010560a02
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-20 18:48:51 +0200

    Save canvas info to canvasEnv in setupCanvas method

--- script/script.js
+++ script/script.js
@@ -1,10 +1,16 @@
 import { fetchWords, changeFetchWordStatus } from "./api.js";
+import { canvasEnv } from "./config.js";
 
 export function setupCanvas(canvas) {
     let [width, height] = [window.innerWidth, window.innerHeight - 100];
 
     canvas.width = width;
     canvas.height = height;
+
+    canvasEnv.canvas = canvas;
+    canvasEnv.ctx = canvas.getContext("2d");
+    canvasEnv.width = width;
+    canvasEnv.height = height;
 
     return [width, height];
 }
