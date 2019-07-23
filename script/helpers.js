9f53335292963089182cecb9bb224cec05772ca3
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-23 17:52:41 +0200

    Move sortBy to top of helpers.js

--- script/helpers.js
+++ script/helpers.js
@@ -6,6 +6,14 @@ export function randomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+export function sortBy(field, ascending = true) {
+    return function(array) {
+        return array.sort((a, b) =>
+            ascending ? a[field] - b[field] : b[field] - a[field]
+        );
+    };
+}
+
 export function formatDate(date) {
     const months = [
         "January",
@@ -29,12 +37,4 @@ export function formatDate(date) {
 
 export function formatHighscore(highscore) {
     return `${highscore.score} &mdash; <em>${highscore.date}</em>`;
-}
-
-export function sortBy(field, ascending = true) {
-    return function(array) {
-        return array.sort((a, b) =>
-            ascending ? a[field] - b[field] : b[field] - a[field]
-        );
-    };
 }
