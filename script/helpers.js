a4c2b6eae55c1f7ef15bc91b9e61875a856133a9
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-23 17:41:52 +0200

    Add sortBy helper function

--- script/helpers.js
+++ script/helpers.js
@@ -30,3 +30,11 @@ export function formatDate(date) {
 export function formatHighscore(highscore) {
     return `${highscore.score} &mdash; <em>${highscore.date}</em>`;
 }
+
+export function sortBy(field, ascending = true) {
+    return function(array) {
+        return array.sort((a, b) =>
+            ascending ? a[field] - b[field] : b[field] - a[field]
+        );
+    };
+}
